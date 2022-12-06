c632e9530eb11e74f207fff9a8b03cde97c38929
Anurag Singh <anurag.comm@gmail.com>
2022-12-06 16:36:31 +0530

    add punjabi and hindi

---
+++ hi/run.sh
@@ -0,0 +1,33 @@
+python run_speech_recognition_seq2seq_streaming.py \
+    --model_name_or_path="openai/whisper-small" \
+    --dataset_name="mozilla-foundation/common_voice_11_0" \
+    --dataset_config_name="hi" \
+    --language="hindi" \
+    --train_split_name="train+validation" \
+    --eval_split_name="test" \
+    --model_index_name="Whisper Small Hindi" \
+    --max_steps="3000" \
+    --output_dir="./" \
+    --per_device_train_batch_size="64" \
+    --per_device_eval_batch_size="32" \
+    --logging_steps="25" \
+    --learning_rate="1e-5" \
+    --evaluation_strategy="steps" \
+    --eval_steps="500" \
+    --save_strategy="steps" \
+    --save_steps="500" \
+    --generation_max_length="225" \
+    --length_column_name="input_length" \
+    --max_duration_in_seconds="30" \
+    --text_column_name="sentence" \
+    --freeze_feature_encoder="False" \
+    --report_to="tensorboard" \
+    --gradient_checkpointing \
+    --fp16 \
+    --overwrite_output_dir \
+    --do_train \
+    --do_eval \
+    --predict_with_generate \
+    --do_normalize_eval \
+    --use_auth_token \
+    --push_to_hub
